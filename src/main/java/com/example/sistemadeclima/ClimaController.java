b2fa486d8acf56eb8c7d9272b7c14f466a9c791d
wanderboy131 <munozwanderley131@gmail.com>
2026-04-18 12:15:44 -0500

    Cambio de CORS

--- src/main/java/com/example/sistemadeclima/ClimaController.java
+++ src/main/java/com/example/sistemadeclima/ClimaController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://wanderboy131.github.io/")
 @RestController
 public class ClimaController {
 
